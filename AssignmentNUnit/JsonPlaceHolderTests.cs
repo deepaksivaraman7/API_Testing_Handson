7b8a2bef92b7f448dd8fbadc93ced580af3cd2f7
deepaksivaraman7 <deepak.sivaraman@ust.com>
2023-12-12 15:36:41 +0530

    12-12-2023

---
+++ AssignmentNUnit/JsonPlaceHolderTests.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+
+namespace AssignmentsNUnit
+{
+    internal class JsonPlaceHolderTests
+    {
+        private RestClient client;
+        private string baseUrl = "https://jsonplaceholder.typicode.com/";
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new RestClient(baseUrl);
+        }
+
+        [Test, Order(2)]
+        public void GetSinglePost()
+        {
+            var request = new RestRequest("posts/2", Method.Get);
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            var post = JsonConvert.DeserializeObject<PostData>(response.Content);
+
+            Assert.NotNull(post);
+            Assert.That(post.Id, Is.EqualTo(2));
+            Assert.IsNotEmpty(post.Title);
+        }
+        [Test, Order(1)]
+        public void CreatePost()
+        {
+            var request = new RestRequest("users", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(new { userId = 100, title="new title", body="new body" });
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+            var post = JsonConvert.DeserializeObject<PostData>(response.Content);
+
+            Assert.NotNull(post);
+            Assert.NotNull(post.Id);
+        }
+        [Test, Order(3)]
+        public void UpdatePost()
+        {
+            var request = new RestRequest("posts/2", Method.Put);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(new { userId = 100, title = "new title", body = "new body" });
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            var post = JsonConvert.DeserializeObject<PostData>(response.Content);
+
+            Assert.NotNull(post);
+            Assert.That(post.Id,Is.EqualTo(2));
+        }
+        [Test, Order(4)]
+        public void DeletePost()
+        {
+            var request = new RestRequest("posts/2", Method.Delete);
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.That(response.Content,Is.EqualTo("{}"));
+        }
+        [Test, Order(5)]
+        public void GetNonExistingPost()
+        {
+            var request = new RestRequest("posts/999", Method.Get);
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.NotFound));
+        }
+        [Test, Order(6)]
+        public void GetAllPosts()
+        {
+            var request = new RestRequest("users", Method.Get);
+            request.AddQueryParameter("page", "1");
+            request.AddHeader("Content-Type", "application/json");
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.NotNull(response.Content);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
--- RestExampleNUnit/ReqResApiTests.cs
+++ RestExampleNUnit/ReqResApiTests.cs
@@ -20,7 +20,7 @@ namespace RestExampleNUnit
             client = new RestClient(baseUrl);
         }
 
-        [Test]
+        [Test, Order(2)]
         public void GetSingleUser()
         {
             var request = new RestRequest("users/2", Method.Get);
@@ -32,6 +32,57 @@ namespace RestExampleNUnit
             Assert.NotNull(user);
             Assert.That(user.Id,Is.EqualTo(2));
             Assert.IsNotEmpty(user.Email);
+        }
+        [Test, Order(6)]
+        public void GetAllUsers()
+        {
+            var request = new RestRequest("users", Method.Get);
+            request.AddQueryParameter("page", "1");
+            request.AddHeader("Content-Type", "application/json");
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.NotNull(response.Content);
+            Console.WriteLine(response.Content);
+        }
+        [Test,Order(1)]
+        public void CreateUser()
+        {
+            var request = new RestRequest("users", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(new { name = "John Doe", job = "Software Developer" });
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+            var user = JsonConvert.DeserializeObject<UserData>(response.Content);
+
+            Assert.NotNull(user);
+            Assert.IsNull(user.Email);
+        }
+        [Test, Order(3)]
+        public void UpdateUser()
+        {
+            var request = new RestRequest("users/2", Method.Put);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(new { name = "Updated John Doe", job = "Senior Software Developer" });
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            var user = JsonConvert.DeserializeObject<UserData>(response.Content);
+
+            Assert.NotNull(user);
+            Assert.IsNull(user.Email);
+        }
+        [Test, Order(4)]
+        public void DeleteUser()
+        {
+            var request = new RestRequest("users/2", Method.Delete);
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.NoContent));
+        }
+        [Test, Order(5)]
+        public void GetNonExistingUser()
+        {
+            var request = new RestRequest("users/999", Method.Get);
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.NotFound));
         }
     }
 }
